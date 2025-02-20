a36f2a10fb4fad91de6b7665b0ad0db8eb1d9b55
Jonatán Paes <jonatanpaes182@gmail.com>
2025-02-19 22:49:30 -0300

    feat: Add Home and SubscriptionForm components for event registration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,8 @@
-import { ArrowRight, Mail, Radio, User } from 'lucide-react'
+import { Radio } from 'lucide-react'
 import Image from 'next/image'
 
-import { Button } from '@/components/button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-
 import logo from '../assets/logo.svg'
+import { SubscriptionForm } from './subscription-form'
 
 export default function Home() {
   return (
@@ -37,29 +35,8 @@ export default function Home() {
             Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
           </p>
         </div>
-        <form className="w-full space-y-6 rounded-2xl border border-gray-600 bg-gray-700 p-8 md:max-w-[440px]">
-          <h2 className="font-heading text-xl font-semibold text-gray-200">
-            Inscrição
-          </h2>
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-              <InputField type="text" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-          <Button type="submit">
-            Confirmar
-            <ArrowRight className="size-6" />
-          </Button>
-        </form>
+
+        <SubscriptionForm />
       </div>
     </div>
   )
