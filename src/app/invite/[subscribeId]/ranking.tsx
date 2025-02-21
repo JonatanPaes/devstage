ad0c935de1c1dda75bb47687bdb43c574d336c42
Jonatán Paes <jonatanpaes182@gmail.com>
2025-02-20 23:27:59 -0300

    feat: Update Ranking component to fetch and display dynamic ranking data

--- src/app/invite/[subscribeId]/ranking.tsx
+++ src/app/invite/[subscribeId]/ranking.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 
+import { getRanking } from '@/http/api'
+
 import medalCooper from '../../../assets/medal-cooper.svg'
 import medalGold from '../../../assets/medal-gold.svg'
 import medalSilver from '../../../assets/medal-silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="font-heading text-xl leading-none font-semibold text-gray-200">
@@ -12,50 +16,53 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="relative flex flex-col justify-center gap-3 rounded-xl border border-gray-600 bg-gray-700 p-6">
-          <span className="text-sm leading-none text-gray-300">
-            <span className="font-semibold">1&ordm;</span> | Jonatán Paes
-          </span>
-          <span className="font-heading text-2xl leading-none font-semibold text-gray-200">
-            1030
-          </span>
+        {ranking.map((rank, index) => {
+          const rankingPosition = index + 1
 
-          <Image
-            src={medalGold}
-            alt="medalGold"
-            className="absolute top-0 right-8"
-          />
-        </div>
+          return (
+            <div
+              key={rank.id}
+              className="relative flex flex-col justify-center gap-3 rounded-xl border border-gray-600 bg-gray-700 p-6"
+            >
+              <span className="text-sm leading-none text-gray-300">
+                <span className="font-semibold">{rankingPosition}&ordm;</span> |{' '}
+                {rank.name}
+              </span>
 
-        <div className="relative flex flex-col justify-center gap-3 rounded-xl border border-gray-600 bg-gray-700 p-6">
-          <span className="text-sm leading-none text-gray-300">
-            <span className="font-semibold">2&ordm;</span> | Jonatán Paes
-          </span>
-          <span className="font-heading text-2xl leading-none font-semibold text-gray-200">
-            1030
-          </span>
+              <span className="font-heading text-2xl leading-none font-semibold text-gray-200">
+                {rank.score}
+              </span>
 
-          <Image
-            src={medalSilver}
-            alt="medalSilver"
-            className="absolute top-0 right-8"
-          />
-        </div>
+              {rankingPosition === 1 && (
+                <Image
+                  src={medalGold}
+                  alt=""
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-        <div className="relative flex flex-col justify-center gap-3 rounded-xl border border-gray-600 bg-gray-700 p-6">
-          <span className="text-sm leading-none text-gray-300">
-            <span className="font-semibold">3&ordm;</span> | Jonatán Paes
-          </span>
-          <span className="font-heading text-2xl leading-none font-semibold text-gray-200">
-            1030
-          </span>
+              {rankingPosition === 2 && (
+                <Image
+                  src={medalSilver}
+                  alt=""
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <Image
-            src={medalCooper}
-            alt="medalCooper"
-            className="absolute top-0 right-8"
-          />
-        </div>
+              {rankingPosition === 3 && (
+                <Image
+                  src={medalCooper}
+                  alt=""
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
+
+        {ranking.length === 0 && (
+          <p className="text-gray-300">Nenhuma indicação registrada</p>
+        )}
       </div>
     </div>
   )
